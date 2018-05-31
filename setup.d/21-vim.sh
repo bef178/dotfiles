cf57ba4f46b8ae9e7b84f0ce5b5981e100d4c1a6
tanghao <tanghao.hb@gmail.com>
2018-05-31 12:37:19 +0000

    remove vim vundle and reorg

--- setup.d/21-vim.sh
+++ setup.d/21-vim.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-root=$1
+DROOT=$1
 
 sudo apt-get install -y vim
 
-if test ! -e $HOME/.vim; then
-    mkdir -p $HOME/.vim
-    ln -s $HOME/.vim/vimrc $HOME/.vimrc
-    ln -s $root/vim/vimrc $HOME/.vim/vimrc
-    ln -s $root/vim/vundle.vim $HOME/.vim/vundle.vim
-    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-    mkdir -p $HOME/.vim/colors
-    cp $root/vim/colors/*.vim $HOME/.vim/colors
-fi
+ln -snf $DROOT/res/vim/vimrc $HOME/.vimrc
+
+mkdir -p $HOME/.vim/colors
+cp $DROOT/res/vim/colors/*.vim $HOME/.vim/colors/
