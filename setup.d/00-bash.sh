2eb05ccb99ab37f405430e1b3024f0e16828079f
tanghao <tanghao.hb@gmail.com>
2018-06-01 00:03:47 +0800

    add common dir

--- setup.d/00-bash.sh
+++ setup.d/00-bash.sh
@@ -3,4 +3,18 @@
 # bashrc sourced by interactive & non-login bash
 
 DROOT="$1"
+
 echo "source \"$DROOT/res/bashrc\"" >> $HOME/.bashrc
+
+mkdir -p $HOME/.ssh
+ln -snf $DROOT/res/ssh-config $HOME/.ssh/config
+
+mkdir $HOME/app
+
+mkdir $HOME/bin
+ln -s /usr/bin/gnome-calculator bin/calc
+ln -s /usr/bin/gnome-terminal bin/cmd
+ln -s /usr/bin/google-chrome bin/chrome
+
+mkdir $HOME/pub
+ln -snf $HOME/pub $HOME/Downloads
