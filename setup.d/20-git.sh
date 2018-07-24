b487b9cc54c9634bd2adc5804a70eb0b3eeb6b4b
tanghao <tanghao.hb@gmail.com>
2018-07-24 20:21:37 +0800

    add ppa for latest stable git

--- setup.d/20-git.sh
+++ setup.d/20-git.sh
@@ -2,6 +2,10 @@
 
 TOP=$(realpath $(pwd)/$(dirname $0)/..)
 
+sudo add-apt-repository -y ppa:git-core/ppa
+
+sudo apt-get update
+
 sudo apt-get install -y git
 
 if test ! -e $HOME/.gitconfig; then
