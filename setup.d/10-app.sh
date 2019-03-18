c2a7d07b1eceea7246dc8818b466f17cd11877b7
tanghao <tanghao.hb@gmail.com>
2019-03-19 00:10:07 +0800

    install ascii

--- setup.d/10-app.sh
+++ setup.d/10-app.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y apt aptitude
 # editor
 sudo apt-get install -y sublime-text
 
-sudo apt-get install -y axel rar unrar testdisk tree p7zip-full
+sudo apt-get install -y axel rar unrar testdisk tree p7zip-full ascii
 
 sudo apt-get install -y astyle expect
 
