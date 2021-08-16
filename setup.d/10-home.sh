b5895f597b44bd61c891db4b6627aa94b1d8ad73
Tang, Hao <tanghao.hb@gmail.com>
2021-08-16 14:34:59 +0800

    draw off win scripts

--- setup.d/10-home.sh
+++ setup.d/10-home.sh
@@ -6,48 +6,27 @@ else
     TOP=$(realpath $(pwd)/$(dirname $BASH_SOURCE)/..)
 fi
 
-if [[ `uname -a` == *Microsoft* ]]; then
-    # wsl
-    export WIN_USER=`cmd.exe /C "echo %USERNAME%" | tr -d '\r'`
-fi
-
-################
-# directory
-#
-
-if test -n "$WIN_USER"; then
-    umask 022
-fi
-
 rmdir --ignore-fail-on-non-empty $HOME/{Documents,Movie,Music,Pictures,Public,Templates,Videos}
-if test -n "$WIN_USER"; then
-    # link Downloads to windows' Downloads folder
-    rmdir --ignore-fail-on-non-empty $HOME/Downloads
-    ln -snf "/mnt/c/Users/${WIN_USER}/Downloads" $HOME/Downloads
-fi
-if test ! -e $HOME/pub; then
-    ln -snf $HOME/Downloads $HOME/pub
-fi
 
 mkdir -p $HOME/bin
+cat >> $HOME/.bashrc << EOF
 
-################
-# dotfiles
-#
+export PATH=\$HOME/bin:\$PATH
+EOF
+
+if [[ `uname -a` == *Microsoft* ]]; then
+    # wsl
+    source $TOP/setup.d/win.10-home.sh
+fi
 
 # .bashrc sourced by interactive & non-login bash
 # TODO be reentrant
-echo >> $HOME/.bashrc
 cat >> $HOME/.bashrc << EOF
+
 source $TOP/res/alias.sh
 source $TOP/res/term-palette-solarized.sh
 source $TOP/res/ps1.sh
 source $TOP/res/gui-settings.sh
-EOF
-
-cat >> $HOME/.bashrc << EOF
-
-export PATH=\$HOME/bin:\$PATH
 EOF
 
 cat >> $HOME/.inputrc << EOF
