690461c685c04423ebf770c97055cb9bb7ea32b4
tanghao <tanghao.hb@gmail.com>
2022-02-18 11:34:29 +0800

    update nvm version and use node v14

--- setup.d/44-nodejs.sh
+++ setup.d/44-nodejs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # nodejs via nvm https://github.com/nvm-sh/nvm#install--update-script
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # this loads nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install --lts
+#nvm install --lts
+nvm install lts/fermium # v14.19.0
 npm install --global yarn
