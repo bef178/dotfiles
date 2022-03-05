a3f3cdf10477cf052febcfb7cb4c01014585b73c
tanghao <tanghao.hb@gmail.com>
2022-03-05 21:52:36 +0800

    map capslock => ctrl_r() for input source shortcut

---
+++ setup.d/14-xmodmap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$BASH_SOURCE" == /* ]]; then
+    TOP=$(realpath $(dirname $BASH_SOURCE)/..)
+else
+    TOP=$(realpath $(pwd)/$(dirname $BASH_SOURCE)/..)
+fi
+
+ln -s $TOP/res.d/Xmodmap ~/.Xmodmap
